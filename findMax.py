bb269c32b4ed198b6e3fceac0f55ca1e6831c2d6
MD. yasin Arafat Mubin <mubinulislam14@gmail.com>
2025-10-11 03:42:23 +0600

    solve findMax

--- findMax.py
+++ findMax.py
@@ -6,6 +6,14 @@ def findMax(arr):
             large = i 
         
     print(large)
+# def findMax(arr):
+
+#     large = arr[0]
+#     for i in arr: 
+#         if i > large:
+#             large = i 
+        
+#     print(large)
 
 
 findMax([1,9,3,4,5,6,7])
